8f445f73d160bcb78be8fbbec772967e7fde427e
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-04-14 12:05:27 +0200

    Prefer system check which result in a new recipe should be treated as normal packages
    In general when are remote store is provided, we disable system packages. However, this
    should not be the case if the system package results in a new recipe, because in that case
    we consider the result of that recipe to be the package.

--- alibuild_helpers/utilities.py
+++ alibuild_helpers/utilities.py
@@ -504,8 +504,8 @@ def getPackageList(packages, specs, configDir, preferSystem, noSystem,
       noSystemList = [spec["package"]]
     elif noSystem is not None:
       noSystemList = noSystem.split(",")
-
-    if (spec["package"] not in noSystemList) and (preferSystem or systemREMatches):
+    excluded = (spec["package"] not in noSystemList)
+    if (preferSystem or systemREMatches):
       requested_version = resolve_version(spec, defaults, "unavailable", "unavailable")
       cmd = "REQUESTED_VERSION={version}\n{check}".format(
         version=quote(requested_version),
@@ -520,7 +520,7 @@ def getPackageList(packages, specs, configDir, preferSystem, noSystem,
       else:
         # prefer_system_check succeeded; this means we should use the system package.
         match = re.search(r"^alibuild_system_replace:(?P<key>.*)$", output, re.MULTILINE)
-        if not match:
+        if not match and not excluded:
           # No replacement spec name given. Fall back to old system package
           # behaviour and just disable the package.
           systemPackages.add(spec["package"])
